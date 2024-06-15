95141832d0d24aa9c7001daa87a733c798720185
w314 <piroska.werner@gmail.com>
2024-06-15 13:32:23 -0500

    Add BrowserROuter to project

--- react-practice-front/src/App.tsx
+++ react-practice-front/src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/Counter" element={<h1>Counter</h1>} />
+      </Routes>
+
     </div>
   );
 }
