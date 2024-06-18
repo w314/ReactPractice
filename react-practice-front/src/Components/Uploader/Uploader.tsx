f47666b322a80d75c9def835655842604e0a8c7b
w314 <piroska.werner@gmail.com>
2024-06-17 19:42:45 -0500

    feat: Uploader Component added

---
+++ react-practice-front/src/Components/Uploader/Uploader.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './Uploader.css'
+
+
+const Uploader: React.FC<{}> = () => {
+
+    // const [ file, setFile ] = React.useState(null);
+    const [ url, setUrl ] = React.useState("");
+
+    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        if(event.target.files && event.target.files.length > 0){
+            const fileInput = event.target.files[0]
+            const url = URL.createObjectURL(fileInput);
+            setUrl(url)
+        }
+    
+    }
+
+    return (
+        <div className="uploader">
+            { url && <img src={url} alt='image' />}
+        <input
+            type="file"
+            onChange={handleFileChange}
+            />    
+        </div>
+    )
+}
+
+export default Uploader
