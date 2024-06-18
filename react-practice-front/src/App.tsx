0e4bd83af8d4dd22f2831189dbc099e7ec897298
w314 <piroska.werner@gmail.com>
2024-06-17 21:03:45 -0500

    style Add styling

--- react-practice-front/src/App.tsx
+++ react-practice-front/src/App.tsx
@@ -7,6 +7,10 @@ import Layout from './Components/Layout/Layout';
 import Counter from './Components/Counter/Counter';
 import TodoList from './Components/TodoList/TodoList';
 import FormValidation from './Components/FormValidation/FormValidation';
+import Search from './Components/Search/Search';
+import Uploader from './Components/Uploader/Uploader';
+import Users from './Components/Users/Users';
+import Profile from './Components/Profile/Profile';
 
 function App() {
   return (
@@ -15,7 +19,11 @@ function App() {
         <Route path="/" element={< Layout />}>
           <Route path="/counter" element={<Counter />}></Route>
           <Route path="/todoList" element={<TodoList />}></Route>  
-          <Route path="/formValidation" element={<FormValidation />}></Route>     
+          <Route path="/formValidation" element={<FormValidation />}></Route>    
+          <Route path='/search' element={<Search />}></Route> 
+          <Route path='/uploader' element={<Uploader />}></Route>
+          <Route path='/users' element={<Users />}></Route>
+          <Route path='/profile' element={<Profile />}></Route>
         </Route>
       </Routes>
 
